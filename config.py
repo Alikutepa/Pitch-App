545fa3e485b227b862840118ba094c12e6b65bbc
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-11-10 18:53:09 +0300

    update changes

---
+++ config.py
@@ -0,0 +1,48 @@
+import os
+
+class Config:
+    """
+    General configuration class
+    """
+    SECRET_KEY = 'Iu0oqP1iaBPV4a6sGP-5IQ'
+    
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    # Email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+
+class ProdConfig(Config):
+    """
+    Production configuration class
+    """
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:access@localhost/pitches_test'
+
+
+
+class DevConfig(Config):
+    """
+    Development configuration class
+    """
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:access@localhost/pitches'
+    DEBUG = True
+    
+
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
+
